84fce94bf46eff00ab747503e40ca2dc54e54bcc
Azariah <89634133+azariahpundari1@users.noreply.github.com>
2023-05-15 21:06:29 +1000

    Add basic php/html boiler code

--- trending.php
+++ trending.php
@@ -1,1 +1,15 @@
-<?php
+<?php ?>
+<html>
+    <head>
+        <title>Trending</title>
+        <meta charset="UTF-8">
+        <link href="styles/style.css" rel="stylesheet">
+    </head>
+
+    <?php include("header.php");
+    include("nav.php")?>
+    
+    <body>
+
+    </body>
+</html>
