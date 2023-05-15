2a39954eb6e7f5b84c5509e54cad780304655b23
Azariah <89634133+azariahpundari1@users.noreply.github.com>
2023-05-15 21:29:27 +1000

    Refactor heading to update when page is changed.

--- header.php
+++ header.php
@@ -1,8 +1,24 @@
 <?php
 $file_name = substr(strchr($_SERVER['SCRIPT_NAME'], "/"), 1);
 $name = substr($file_name, 0, strrpos($file_name, "."));
+$page_title = '';
+$logoImage = 'images/logo.png';
+echo $name
 ?>
 <header>
-    <h1>Azariah's Favourite Songs</h1>
-    <img src="images/logo.png" alt="Music logo">
+    <?php
+        if ($name == 'basic-website/index') {
+            $page_title = "Azariah's Favourite Songs";
+        }
+        elseif ($name == 'basic-website/trending') {
+            $page_title = 'Trending';
+        } elseif ($name == 'basic-website/categories') {
+            $page_title = 'Categories';
+        } else {
+            $page_title = '404 page not found';
+            $logoImage = '';
+        }
+    ?>
+    <h1><?php echo $page_title; ?></h1>
+    <img src= <?php echo $logoImage; ?>  alt="" alt="Page Logo">
 </header>
