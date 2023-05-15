316e80751bcda45f896c07a1e3a235b398c35b02
Azariah <89634133+azariahpundari1@users.noreply.github.com>
2023-05-15 20:40:49 +1000

    Add li elements with corresponding names

--- nav.php
+++ nav.php
@@ -1,4 +1,10 @@
 <?php
-$file_name = substr(strchr($_SERVER['SCRIPT_NAME'], "/"), 1);
-$name = substr($file_name, 0, strrpos($file_name, "."));
-echo $name;
+global $name; ?>
+<nav>
+  <ul>
+    <li <?php if ($name == "index") echo 'class="current"'; ?>><a href="index.php"><span class="glyphicon glyphicon-home"></span> Home</a></li>
+    <li <?php if ($name == "header") echo 'class="current"'; ?>><a href="header.php"><span class="glyphicon glyphicon-star"></span> Advisors</a></li>
+    <li <?php if ($name == "categories") echo 'class="current"'; ?>><a href="categories.php""><span class="glyphicon glyphicon-comment"></span> Sample Advice</a></li>
+    <li <?php if ($name == "about") echo 'class="current"'; ?>><a href="about.php"><span class="glyphicon glyphicon-shopping-cart"></span> Subscribe Now</a></li>
+  </ul>
+</nav>
