588f73c293f4400d6cab02ecca8848e38c7e16af
Azariah <89634133+azariahpundari1@users.noreply.github.com>
2023-05-15 20:41:12 +1000

    Add h1 heading & logo image

--- header.php
+++ header.php
@@ -1,1 +1,9 @@
 <?php
+$file_name = substr(strchr($_SERVER['SCRIPT_NAME'], "/"), 1);
+$name = substr($file_name, 0, strrpos($file_name, "."));
+echo $name;
+?>
+<header>
+    <h1>The music List</h1>
+    <img src="images/logo.png" alt="Music logo">
+</header>
