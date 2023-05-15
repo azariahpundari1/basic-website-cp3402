7d8f0d7cbd9c2a2f59ab2db402e61cfe8b8da1e6
Azariah <89634133+azariahpundari1@users.noreply.github.com>
2023-05-15 19:03:43 +1000

    Add basic content for main page

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php ?>
+<!doctype html>
+<html lang="eng">
+    <head>
+        <title>Basic Website Using Sass</title>
+        <meta charset="UTF-8">
+        <link href="styles/style.css" rel="stylesheet" type="text/css">
+    </head>
+
+    <header>
+        <h1>Music List/h1>
+    </header>
+
+</html>
