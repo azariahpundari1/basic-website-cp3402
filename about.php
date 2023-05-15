9d372923c6fd963a70e7f5e5fe026da7557e5609
Azariah <89634133+azariahpundari1@users.noreply.github.com>
2023-05-15 22:12:26 +1000

    Add about content

--- about.php
+++ about.php
@@ -10,6 +10,10 @@
 include("nav.php")?>
 
 <body>
+    <p>
+        This is just a simple website displaying my favourite songs. They change everytime hence why they're updated
+        every week. There will be future updates to display them dynamically everytime my taste in music changes.
+    </p>
 
 </body>
 </html>
