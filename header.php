c39d2d83fccb729c20c5c874472959b301dacc63
Azariah <89634133+azariahpundari1@users.noreply.github.com>
2023-05-15 21:05:29 +1000

    Refactor h1 heading

--- header.php
+++ header.php
@@ -1,9 +1,8 @@
 <?php
 $file_name = substr(strchr($_SERVER['SCRIPT_NAME'], "/"), 1);
 $name = substr($file_name, 0, strrpos($file_name, "."));
-echo $name;
 ?>
 <header>
-    <h1>The music List</h1>
+    <h1>Azariah's Favourite Songs</h1>
     <img src="images/logo.png" alt="Music logo">
 </header>
