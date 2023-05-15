f15c5c73f5c2f4a9ce2d59792ff290148ac5aac1
Azariah <89634133+azariahpundari1@users.noreply.github.com>
2023-05-15 20:44:37 +1000

    Refactor li element - trending

--- nav.php
+++ nav.php
@@ -3,8 +3,8 @@ global $name; ?>
 <nav>
   <ul>
     <li <?php if ($name == "index") echo 'class="current"'; ?>><a href="index.php"><span class="glyphicon glyphicon-home"></span> Home</a></li>
-    <li <?php if ($name == "header") echo 'class="current"'; ?>><a href="header.php"><span class="glyphicon glyphicon-star"></span> Advisors</a></li>
-    <li <?php if ($name == "categories") echo 'class="current"'; ?>><a href="categories.php""><span class="glyphicon glyphicon-comment"></span> Sample Advice</a></li>
-    <li <?php if ($name == "about") echo 'class="current"'; ?>><a href="about.php"><span class="glyphicon glyphicon-shopping-cart"></span> Subscribe Now</a></li>
+    <li <?php if ($name == "trending") echo 'class="current"'; ?>><a href="trending.php"><span class="glyphicon glyphicon-star"></span>Trending</a></li>
+    <li <?php if ($name == "categories") echo 'class="current"'; ?>><a href="categories.php""><span class="glyphicon glyphicon-comment"></span> Categories</a></li>
+    <li <?php if ($name == "about") echo 'class="current"'; ?>><a href="about.php"><span class="glyphicon glyphicon-shopping-cart"></span> About Us</a></li>
   </ul>
 </nav>
