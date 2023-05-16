7ce540fa47b3c487875ba757152651091c5ed5dc
Azariah <89634133+azariahpundari1@users.noreply.github.com>
2023-05-16 16:06:00 +1000

    Refactor to display paragraph in two lines

--- about.php
+++ about.php
@@ -3,17 +3,22 @@
 <head>
     <title>About Us</title>
     <meta charset="UTF-8">
-    <link href="styles/style.css" rel="stylesheet">
+    <link href="styles/style.css" rel="stylesheet" type="text/css">
 </head>
 
 <?php include("header.php");
 include("nav.php")?>
 
 <body>
+    <div class="content">
     <p>
         This is just a simple website displaying my favourite songs. They change everytime hence why they're updated
+    </p>
+    <p>
         every week. There will be future updates to display them dynamically everytime my taste in music changes.
     </p>
+
+    </div>
 
 </body>
 </html>
